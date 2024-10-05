9e6a8cc188b2e54d820ea47d64fbeb30dd32a719
neinu <noms22@hotmail.com>
2024-10-05 12:08:30 +0200

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,49 @@
 function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
-    let cityElement = document.querySelector("#weather-app-city");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time *1000);
 
     cityElement.innerHTML = response.data.city;
- temperatureElement.innerHTML = Math.round(temperature);
+
+    timeElement.innerHTML = formatDate(date); 
+    descriptionElement.innerHTML = response.data.condition.discription;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    temperatureElement.innerHTML = Math.round(temperature);
+
+    function formatDate(date) {
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let days = [
+            "sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Sartuday",
+        ];
+        let day = days[date.getDay()];
+
+        if (minutes < 10) {
+            minutes = `0${mitutes}`;
+
+        }
+
+
+
+
+        return `${day} ${hours} ${minutes}`;
+
+    }
+    
+
+
 
 }
 
